442dd2c85b2791a057f9590d175e573dd9c8e357
mattcy0514 <b10809024@mail.ntust.edu.tw>
2022-04-19 20:08:57 +0800

    cv-migrate-with-algo-v1

--- pool/server.py
+++ pool/server.py
@@ -3,15 +3,19 @@ import pool
 import numpy as np
 from sprite import Ball
 import json
+import cv
 app = Flask(__name__)
 
-@app.route("/path", methods=['POST'])
+@app.route("/path", methods=['POST', 'GET'])
 def path():
+    balls_json = None
     if request.method == 'POST':
         balls_json = request.get_json()
-        if type(balls_json) == str:
-            balls_json = json.loads(balls_json)
-        print(type(balls_json))
+    elif request.method == 'GET':
+        balls_json = cv.main()
+    
+    if type(balls_json) == str:
+        balls_json = json.loads(balls_json)
         mball = None
         tballs = []
         for i in balls_json:
@@ -21,5 +25,7 @@ def path():
             else:
                 tballs.append(Ball(np.matrix(ball_pos)))
         return pool.pool(mball, tballs, 4, 1)
+    return "BAD", 400
+
 if __name__ == '__main__':
     app.run('0.0.0.0', 5555)
